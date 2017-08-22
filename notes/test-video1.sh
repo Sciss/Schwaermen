831f8e3bc2f5cd092198d7878cda3fb0cb0efab7
Hanns Holger Rutz <contact@sciss.de>
2017-08-22 04:13:25 +0200

    ...

--- notes/test-video1.sh
+++ notes/test-video1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # schwaermen-video --laptop --own-socket 127.0.1.1:55000 --dot 1 --video-id 0 --small-window --video-sockets 127.0.1.1:55001:3 --sound-sockets 127.0.1.1:55002:66 --speakers notes/test-speakers.txt --log
-schwaermen-video --laptop --own-socket 127.0.1.1:55000 --dot 1 --video-id 0 --small-window --video-sockets 127.0.1.1:55001:3 --log
+schwaermen-video --laptop --own-socket 192.168.0.77:55000 --dot 1 --video-id 0 --small-window --video-sockets 192.168.0.77:55001:3 --log
--- notes/test-video2.sh
+++ notes/test-video2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # schwaermen-video --laptop --own-socket 127.0.1.1:55001 --dot 3 --video-id 2 --small-window --video-sockets 127.0.1.1:55000:1 --sound-sockets 127.0.1.1:55002:66 --speakers notes/test-speakers.txt --log
-schwaermen-video --laptop --own-socket 127.0.1.1:55001 --dot 3 --video-id 2 --small-window --video-sockets 127.0.1.1:55000:1 --log
+schwaermen-video --laptop --own-socket 192.168.0.77:55001 --dot 3 --video-id 2 --small-window --video-sockets 192.168.0.77:55000:1 --log
